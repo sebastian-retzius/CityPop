9fa7063bbd734d7b8efac2fc936061350b27d791
Sebastian Retzius <sebre055@student.liu.se>
2021-11-11 15:18:24 +0100

    feat: add styling and buttosn for SearchForCountry component feat: add sending params from serachcountry component to country component

--- Screens/Country.tsx
+++ Screens/Country.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { RootStackParamList } from '../Types/RootStackParamList';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-export default function Country() {
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Country'>
+
+export default function Country({route}: Props) {
   return (
     <View>
-      <Text>Country</Text>
+      <Text>Country: {route.params.country}</Text>
     </View>
   )
 }
